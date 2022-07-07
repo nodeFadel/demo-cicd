3b18d33b53d102d21fb2dc4a4791e8726417bc9a
nodeFadel <fadel.a.bourji@gmail.com>
2022-07-07 11:31:37 +0200

    creating index.js file

---
+++ index.js
@@ -0,0 +1,6 @@
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 3000;
+
+app.use(express.static('www'));
+app.listen(port, ()=> console.log('Listening on http://localhost:'+port))
